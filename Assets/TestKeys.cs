5811e5bf3f1b3af9638fa4ec8dfef44e688720f5
Michael Dorian <doby162@gmail.com>
2021-07-13 01:13:51 -0700

    Add delta time to movement

--- Assets/TestKeys.cs
+++ Assets/TestKeys.cs
@@ -3,11 +3,13 @@ using UnityEngine.InputSystem;
 
 public class TestKeys : MonoBehaviour
 {
-    public float speed = 0.1f;
+    public float speed = 1f;
 
     public int maxFrames = 144;
 
-    public float gravity = -.1f;
+    public float gravity = -1f;
+
+    public float rotationSpeed = 1f;
 
     private CharacterController charControl;
 
@@ -30,7 +32,7 @@ public class TestKeys : MonoBehaviour
         {
             //keyboard and mouse controls
             var delts = mouse.delta;
-            transform.Rotate(0f, 1f * delts.x.ReadValue(), 0f);
+            transform.Rotate(0f, rotationSpeed * delts.x.ReadValue() * Time.deltaTime, 0f);
 
             if (keyboard.leftArrowKey.isPressed) transform.Translate(-speed, 0, 0);
 
@@ -46,14 +48,13 @@ public class TestKeys : MonoBehaviour
         }
         else
         {
-            transform.Rotate(0f, 1f * gamepad.rightStick.x.ReadValue(), 0f);
-            // transform.Translate(gamepad.leftStick.x.ReadValue() * -speed, 0, gamepad.leftStick.y.ReadValue() * -speed);
+            transform.Rotate(0f, rotationSpeed * gamepad.rightStick.x.ReadValue() * Time.deltaTime, 0f);
             // all this nonsense allows the char controller to enforce walls
             // see page 57 of unity in action
             var deltaX = gamepad.leftStick.x.ReadValue() * -speed;
             var deltaZ = gamepad.leftStick.y.ReadValue() * -speed;
             var movement = new Vector3(deltaX, gravity, deltaZ);
-            // movement *= Time.deltaTime;
+            movement *= Time.deltaTime;
             movement = transform.TransformDirection(movement);
             charControl.Move(movement);
         }
